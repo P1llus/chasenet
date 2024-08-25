d6f6d8591fc348c8355ba3b057d2ed7014f6490d
Marius Iversen <marius.iversen@elastic.co>
2024-08-25 19:59:15 +0200

    test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/P1llus/chasenet
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Call the function from the package
+	fmt.Println("Hello, World!")
+}
