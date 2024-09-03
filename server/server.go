8b7a08e4068817461bc1fef6110e9656792ffb81
Marius Iversen <marius.iversen@elastic.co>
2024-09-03 18:24:50 +0200

    add middleware for http(s) redirects, add FA and icons (#6)

--- server/server.go
+++ server/server.go
@@ -1,11 +1,21 @@
 package server
 
 import (
+	"os"
+
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func Start() {
 	e := initEcho()
+	if os.Getenv("APP_ENV") == "production" {
+		e.Pre(middleware.HTTPSNonWWWRedirect())
+	}
+	if os.Getenv("APP_ENV") != "production" {
+		e.Pre(middleware.NonWWWRedirect())
+		e.Use(middleware.Logger())
+	}
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
